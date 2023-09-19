6099c1ec3a92f5109684fdef63a05774e51c1419
Johannes Maendle <32535140+jomaendle@users.noreply.github.com>
2023-09-19 21:46:05 +0200

    feat: add content

---
+++ src/content/images/bali-flower.md
@@ -0,0 +1,4 @@
+---
+color: "#5397b1"
+image: "./bali-flower.jpg"
+---
--- src/scripts/image-load.ts
+++ src/scripts/image-load.ts
@@ -5,11 +5,23 @@ import { getCollection } from "astro:content";
  * Variables
  */
 const allImages = await getCollection("images");
-
 const currentImage = allImages[0].data;
 
-const image: HTMLImageElement | null = document.getElementById(
+let isImageLoading = true;
+
+/**
+ * DOM Elements
+ */
+const nextImageButton: HTMLButtonElement = document.getElementById(
+  "nextImageButton"
+) as HTMLButtonElement;
+
+const image: HTMLImageElement = document.getElementById(
   "high-quality-image"
+) as HTMLImageElement;
+
+const bgImage: HTMLImageElement = document.getElementById(
+  "bg-image"
 ) as HTMLImageElement;
 
 let hasLoadedHighQualityImage = false;
@@ -18,14 +30,45 @@ if (!image) {
   throw new Error("Image not found");
 }
 
+/**
+ * Event Listeners
+ */
+nextImageButton.addEventListener("click", async () => {
+  if (isImageLoading) {
+    console.log("Image is loading");
+    return;
+  }
+
+  const nextImage = allImages[Math.floor(Math.random() * allImages.length)];
+
+  const lqImg = await getImage({
+    src: nextImage.data.image,
+    width: 800,
+    quality: "mid",
+  });
+
+  image.src = lqImg.src;
+  bgImage.src = lqImg.src;
+
+  document.documentElement.style.setProperty(
+    "--backgroundColor",
+    nextImage.data.color
+  );
+});
+
 image.addEventListener("load", () => {
   image.classList.add("transition-opacity");
   image.classList.add("duration-500");
   image.classList.add("opacity-100");
 
+  isImageLoading = false;
+
   setTimeout(onImageLoad, 2000);
 });
 
+/**
+ * Functions
+ */
 async function onImageLoad() {
   const highQualityImage = await getImage({
     src: currentImage.image,
@@ -45,4 +88,5 @@ async function onImageLoad() {
   image.classList.add("opacity-0");
   image.classList.remove("opacity-100");
   hasLoadedHighQualityImage = true;
+  isImageLoading = true;
 }
