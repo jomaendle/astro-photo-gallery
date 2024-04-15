623ab1154f0397851f51f7ac1c7441e17cfe5c76
Johannes Maendle <141119154+jomaendle2@users.noreply.github.com>
2024-04-15 14:39:42 +0200

    add astro check

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,8 @@ import db from "@astrojs/db";
 // https://astro.build/config
 export default defineConfig({
   adapter: netlify(),
+  site: "https://photo.jomaendle.de/",
+  prefetch: true,
   output: "server",
   integrations: [tailwind({
     nesting: true
