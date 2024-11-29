82034ad7274cf5f570b5ba64998e36bce9ba62f9
Johannes Maendle <141119154+jomaendle2@users.noreply.github.com>
2024-11-29 16:38:42 +0100

    update deps

--- src/util/images.ts
+++ src/util/images.ts
@@ -2,17 +2,14 @@ import type { ImageWithMeta } from "@/components/ImageCarousel.tsx";
 import { getImage } from "astro:assets";
 import { getCollection, type CollectionEntry } from "astro:content";
 
-export async function loadImages(
-  preferredImageWidth: number,
-  quality?: number
-): Promise<ImageWithMeta[]> {
+export async function loadImages(): Promise<ImageWithMeta[]> {
   const imageCollection = await getCollection("images");
   return await Promise.all(
     imageCollection.map(async (image: CollectionEntry<"images">) => {
       const imageResult = await getImage({
         src: image.data.image,
-        width: preferredImageWidth,
-        quality: quality ?? 80,
+        width: 800,
+        quality: 80,
       });
 
       return {
